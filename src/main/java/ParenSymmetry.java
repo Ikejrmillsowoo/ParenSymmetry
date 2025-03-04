6e47977fd9ab7797a02c781d864792395509caa5
Isaac <isaac@Zipcoders-MacBook-Pro-2.local>
2025-03-04 14:19:38 -0500

    Passed ParenSymetry test and completes read file

--- src/main/java/ParenSymmetry.java
+++ src/main/java/ParenSymmetry.java
@@ -1,20 +1,53 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class ParenSymmetry {
 
     public Boolean isBalanced(String s) {
-        return null;
+        int firstInt = 0;
+        int secondInt = 0;
+
+        while (secondInt < s.length()) {
+            char current = s.charAt(secondInt);
+
+            if (current == '(') {
+                firstInt += 1;
+            } else if (current == ')') {
+                firstInt -= 1;
+            } else if (firstInt < 0) {
+                return false;
+            }
+            secondInt++;
+
+        }
+
+        return (firstInt == 0);
     }
 
     private void checkFile(String filename) {
         // open file named filename
 
-        // for each line in the file
+        try {
+            File file = new File(filename);
+            Scanner scReader = new Scanner(file);
+            // for each line in the file
             // read the line
             // print whether or not the line's parenthesis are balanced
+            while (scReader.hasNextLine()) {
+                String data = scReader.nextLine();
+                System.out.println(isBalanced(data));
+            }
 
-        // CLOSE the file
+            // CLOSE the file
+            scReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
@@ -36,6 +69,8 @@ public class ParenSymmetry {
             trues = ps.isBalanced(strToTest);
         }
         printResult(trues, true);
+
+        ps.checkFile("TestStrings0.txt");
 
     }
 
